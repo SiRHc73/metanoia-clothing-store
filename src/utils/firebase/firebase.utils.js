5b852f63f102cf5f545f1d0d591f42d2a3a7a7d4
SiRHcLaen <cnmadbox@gmail.com>
2022-10-05 22:11:45 -0400

    added sign-in component and Firebase utils

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,51 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBuYnopBPoTo4hFb4GyNAM7ZoDaRck8Dk0",
+  authDomain: "metanoia-clothing.firebaseapp.com",
+  projectId: "metanoia-clothing",
+  storageBucket: "metanoia-clothing.appspot.com",
+  messagingSenderId: "958006602198",
+  appId: "1:958006602198:web:e44085732cde7c6733dc77",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+
+  return userDocRef;
+};
